195176d33d1157092f0b804d11879089e22f4bbf
tailaijin <tailaijin1991@gmail.com>
2016-12-09 14:01:09 -0500

    q
    q

--- public/charts/highcharts/columnBarCtrl.js
+++ public/charts/highcharts/columnBarCtrl.js
@@ -20,7 +20,45 @@ angular.module('charts')
             console.log('Error: ' + data);
           });
       }
-
+      $(function callChart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Stacked column chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                stacking: 'percent'
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1]
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5]
+        }]
+    });
+});
+/*
     function callChart() {
       var state = [];
       var numcolleges = [];
@@ -51,6 +89,6 @@ angular.module('charts')
                 }
             }
         });
-
     }
+*/
     });
