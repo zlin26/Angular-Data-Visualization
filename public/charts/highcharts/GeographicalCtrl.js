c448bf8d40f2c332ed702fd99ee1bb35223fd27e
tailaijin <tailaijin1991@gmail.com>
2016-12-11 13:09:20 -0500

    1
    1

--- public/charts/highcharts/GeographicalCtrl.js
+++ public/charts/highcharts/GeographicalCtrl.js
@@ -217,7 +217,7 @@ angular.module('charts').controller
                     [
                     {
                         data : data,
-                        mapData: Highcharts.maps['countries/us/us-wa-all'],
+                        mapData: Highcharts.maps["countries/us/us-wa-all"],
                         joinBy: 'hc-key',
                         name: 'Random data',
                         states: 
