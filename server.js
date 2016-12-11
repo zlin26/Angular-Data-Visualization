73dcc287310b9642f655266853b0eec293c240f4
tailaijin <tailaijin1991@gmail.com>
2016-12-11 12:22:00 -0500

    1
    1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app      = express(); 								// create our app w/ express
 var port  	 = process.env.PORT || 8090; 				// set the port
 //var database = require('./config/database'); 			// load the database config
 
-//var morgan = require('morgan'); 		// log requests to the console (express4)
+var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
@@ -13,7 +13,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 //mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
-//app.use(morgan('dev')); 										// log every request to the console
+app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
